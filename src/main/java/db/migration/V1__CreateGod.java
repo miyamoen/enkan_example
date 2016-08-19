a93b3e07fc4673122b1762f1bb7e361bb6c298a2
Akihito Terazawa <terazawa.akihito@tis.co.jp>
2016-08-19 17:23:37 +0900

    Godテーブルを作る

--- src/main/java/db/migration/V1__CreateGod.java
+++ src/main/java/db/migration/V1__CreateGod.java
@@ -8,17 +8,19 @@ import java.sql.Statement;
 /**
  * @author terazawa
  */
-public class V1__CreateSurvey  implements JdbcMigration {
+public class V1__CreateGod implements JdbcMigration {
     @Override
     public void migrate(Connection connection) throws Exception {
         try (Statement stmt = connection.createStatement()) {
-            stmt.execute("CREATE TABLE survey " +
+            stmt.execute("CREATE TABLE god " +
                     "(id identity primary key, " +
-                    " item1 VARCHAR(50)," +
-                    " item2 VARCHAR(50)," +
-                    " item3 VARCHAR(50)," +
-                    " item4 VARCHAR(50)," +
-                    " item5 VARCHAR(100))");
+                    " name VARCHAR(30)," +
+                    " description VARCHAR(100)," +
+                    " love INT," +
+                    " promise INT," +
+                    " work INT," +
+                    " life INT," +
+                    " trust INT)");
         }
     }
 }
